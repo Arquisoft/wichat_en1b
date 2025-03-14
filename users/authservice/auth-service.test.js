87883ab6972abd936af4393a8413235a2b935f96
Fernando <uo288583@uniovi.es>
2025-03-14 17:14:21 +0100

    rollback

--- users/authservice/auth-service.test.js
+++ users/authservice/auth-service.test.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 const User = require('./auth-model');
 
 let mongoServer;
-let app = require('./auth-service');
+let app;
 
 //test user
 const user = {
@@ -26,6 +26,7 @@ beforeAll(async () => {
   mongoServer = await MongoMemoryServer.create();
   const mongoUri = mongoServer.getUri();
   process.env.MONGODB_URI = mongoUri;
+  app = require('./auth-service'); 
   //Load database with initial conditions
   await addUser(user);
 });
