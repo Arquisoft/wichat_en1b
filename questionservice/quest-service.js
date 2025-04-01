11b614e38c92e51dfced7de96b5de97a48410a62
Rolitoansu <rolitoansu@outlook.com>
2025-04-01 16:31:08 +0200

    Tests for question service

--- questionservice/quest-service.js
+++ questionservice/quest-service.js
@@ -1,16 +1,17 @@
-const express = require("express")
-const cors  = require("cors")
-const WikidataController = require("./controllers/WikidataController")
+const express = require("express");
+const cors  = require("cors");
+const WikidataController = require("./controllers/WikidataController");
 
-const app = express()
+const app = express();
 const port = 8004
 
 const wikidataController = new WikidataController()
-app.use(express.json())
-app.use(cors())
+
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
-    res.status(200).json({ message: "Server working" })
+    res.status(200).json({ message: "Server working" });
 })
 
 const getWikidataQuestion = async(req, res) => {
--- questionservice/quest-service.test.js
+++ questionservice/quest-service.test.js
@@ -1,18 +1,41 @@
 const request = require('supertest');
 
-let app;
-
-beforeAll(async () => {
-    app = require('./quest-service');
-})
+const app = require('./quest-service');
 
 afterAll(async () => {
     app.close();
 })
 
 describe('Question Service', () => {
-    it('should give a status 200 when a GET request is send to /', async() => {
-        const response = await request(app).get('/');
+    it('Should give a status 200 when a GET request is sent to /', async() => {
+        let response = await request(app).get('/');
         expect(response.status).toBe(200);
-    })
+    });
+
+    it('Should retrieve questions through the /question endpoint', async() => {
+        let response = await request(app).get('/question');
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('id');
+        expect(response.body).toHaveProperty('question');
+        expect(response.body).toHaveProperty('images');
+        expect(response.body.images).toHaveLength(4);
+    });
+
+    it('Should validate correctly an answer', async() => {
+        let response = await request(app).post('/answer', { 
+            answer: 'http://commons.wikimedia.org/wiki/Special:FilePath/Flag%20of%20Lithuania.svg',
+            questionId: 'n80wtegkkgm98b2jfiu'}
+        );
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('correct', true);
+    });
+
+    it('Should fail when the /question endpoint receiven an invalid question type', async() => {
+        let response = await request(app).get('/question/dogs');
+
+        expect(response.status).toBe(500); // In my opinion, this should be a 400 response code
+        expect(response.body).toHaveProperty('error', 'Failed to fetch question');
+    });
 })
