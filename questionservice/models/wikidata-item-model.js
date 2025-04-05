d425112d86fbc3f08165ad2c35b820b672ae4cb6
Br1NKOL <llanera09alb@gmail.com>
2025-04-05 17:02:40 +0200

    models for: wikidata item & question

---
+++ questionservice/models/wikidata-item-model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const wikidataItemSchema = new mongoose.Schema({
+    wikidataId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    label: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    }
+});
+
+const WikidataItem = mongoose.model('WikidataItem', wikidataItemSchema);
+
+module.exports = WikidataItem;
