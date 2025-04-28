41d65373a744e105b69fb816445206a2eb0d2ce1
Rolitoansu <rolitoansu@outlook.com>
2025-04-28 12:19:52 +0200

    Todo

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -2,6 +2,16 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+import "..w/"
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({
+    t: (key) => key,
+    i18n: {
+      changeLanguage: () => new Promise(() => {}),
+    },
+  })
+}));
+
 describe('App Render Tests', () => {
     test('renders welcome message', () => {
       render(
@@ -10,8 +20,8 @@ describe('App Render Tests', () => {
       const wichat = screen.getByText('wichat')
       const _en1b = screen.getByText('_en1b')
       const navBarText = screen.getByText(/wichat_en1b/i)
-      const loginButton = screen.getByText(/Login/i);
-      const signupButton = screen.getByText(/Sign up/i);
+      const loginButton = screen.getByText("navBar.logIn");
+      const signupButton = screen.getByText("navBar.signUp");
 
       expect(wichat).toBeInTheDocument();
       expect(_en1b).toBeInTheDocument();
