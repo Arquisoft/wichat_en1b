e4f0c439a7a8d189291e569537e1738b2777722b
orvizz <marioorvvie@gmail.com>
2025-05-02 15:34:36 +0200

    Modified statitics-service and front-end insights to show the max correct answers of sudden death

--- webapp/src/components/Profile/components/AdditionalInsights.jsx
+++ webapp/src/components/Profile/components/AdditionalInsights.jsx
@@ -2,8 +2,9 @@ import { Typography, Box, Card, CardContent, Divider, Grid } from "@mui/material
 import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
 import { useTranslation } from "react-i18next";
 
-const AdditionalInsights = ({ avgQuestionsPerGame, successRate, maxScore }) => {
+const AdditionalInsights = ({ avgQuestionsPerGame, successRate, maxScore, type, maxStreak }) => {
   const { t } = useTranslation();
+  console.log("AdditionalInsights: type", type);
 
   return (
     <Grid container spacing={3} sx={{ mb: 3 }}>
@@ -15,22 +16,32 @@ const AdditionalInsights = ({ avgQuestionsPerGame, successRate, maxScore }) => {
               <Typography variant="h6">{t("profile.additionalInsights.additionalInsights")}</Typography>
             </Box>
             <Divider sx={{ mb: 2 }} />
-            
+
             <Grid container spacing={3}>
               <Grid item xs={12} md={4}>
+
                 <Box sx={{ mb: { xs: 2, md: 0 } }}>
                   <Typography variant="body2" color="text.secondary">{t("profile.additionalInsights.avgQuestions")}:</Typography>
                   <Typography variant="h5">{avgQuestionsPerGame.toFixed(1)}</Typography>
                 </Box>
+
+
               </Grid>
-              
+
               <Grid item xs={12} md={4}>
-                <Box sx={{ mb: { xs: 2, md: 0 } }}>
-                  <Typography variant="body2" color="text.secondary">{t("profile.additionalInsights.correctAnswerRate")}:</Typography>
-                  <Typography variant="h5">{successRate.toFixed(1)}%</Typography>
-                </Box>
+                {type !== "suddenDeath" ? (
+                  <Box sx={{ mb: { xs: 2, md: 0 } }}>
+                    <Typography variant="body2" color="text.secondary">{t("profile.additionalInsights.correctAnswerRate")}:</Typography>
+                    <Typography variant="h5">{successRate.toFixed(1)}%</Typography>
+                  </Box>
+                ) :
+                  (<Box sx={{ mb: { xs: 2, md: 0 } }}>
+                    <Typography variant="body2" color="text.secondary">{t("profile.additionalInsights.maxStreak")}:</Typography>
+                    <Typography variant="h5">{maxStreak}</Typography>
+                  </Box>
+                  )}
               </Grid>
-              
+
               <Grid item xs={12} md={4}>
                 <Box>
                   <Typography variant="body2" color="text.secondary">{t("profile.additionalInsights.maxScore")}:</Typography>
